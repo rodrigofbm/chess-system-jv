c963b39fd5b538f4bc2fa42617bf57964b4c36da
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2021-09-11 18:02:25 -0300

    BoardException e programação defensiva

--- src/com/company/boardgame/Board.java
+++ src/com/company/boardgame/Board.java
@@ -6,6 +6,10 @@ public class Board {
     private Piece[][] pices;
 
     public Board(int rows, int columns) {
+        if(rows < 1 || columns < 1) {
+            throw new BoardException("Error on creating board: rows and columns should be at least 1");
+        }
+
         this.rows = rows;
         this.columns = columns;
 
@@ -16,28 +20,46 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
-
     public Piece pice(int row, int column) {
+        if(!positionExists(row, column)) {
+            throw new BoardException("invalid Position");
+        }
         return pices[row][column];
     }
 
-    public Piece pice(Position position) {
-        return pices[position.getRow()][position.getColumn()];
+    public Piece piece(Position position) {
+        return pice(position.getRow(),position.getColumn());
     }
 
     public void placePiece(Piece piece, Position position) {
+        if(thereIsAPiece(position)) {
+            throw new BoardException("Slot not empty");
+        }
         pices[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    public boolean positionExists(int row, int column) {
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        if(!positionExists(position.getRow(), position.getColumn())) {
+            throw new BoardException("invalid Position");
+        }
+
+        return piece(position) != null;
+    }
+
+    public boolean thereIsAPiece(int row, int column) {
+        return thereIsAPiece(new Position(row, column));
     }
 }
