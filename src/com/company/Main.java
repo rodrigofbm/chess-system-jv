86902906534a8db74cba9a853e3ab391019c6cc8
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2021-09-11 17:05:22 -0300

    add Position class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import com.company.boardgame.Position;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Position position = new Position(3,5);
+
+        System.out.println(position);
     }
 }
